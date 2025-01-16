8cf1b3e7dce593b03c34fdb32773d94ea31a54e5
Rachhen <rachhen.it@outlook.com>
2025-01-17 00:12:11 +0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ isSsrBuild }) => ({
+export default defineConfig(({ isSsrBuild, command }) => ({
   build: {
     rollupOptions: isSsrBuild
       ? {
@@ -16,6 +16,9 @@ export default defineConfig(({ isSsrBuild }) => ({
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
+  },
+  ssr: {
+    noExternal: command === "build" ? true : undefined,
   },
   plugins: [reactRouter(), tsconfigPaths()],
 }));
